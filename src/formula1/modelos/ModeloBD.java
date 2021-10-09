2a102b710b94cdec7c8db21f6d4ab86d5a57e3dd
505 <505@academicos.cesde.edu.co>
2021-10-08 20:30:51 -0500

    taller de formula 1 creando una aplicacion de carros

---
+++ src/formula1/modelos/ModeloBD.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formula1.modelos;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author 505
+ */
+public class ModeloBD {
+    
+    private String servidor="jdbc:mysql://localhost/formula1";
+    private String usuario="root";
+    private String password="";
+
+    public ModeloBD() {
+    }
+    
+    public Connection conectarBD (){
+    
+    Connection conexion = null;
+    try{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        conexion=DriverManager.getConnection(servidor, usuario,password);
+        System.out.println("Exito conectandonos");
+        return conexion;
+        
+    }catch(Exception error){
+        System.out.println("upss.."+error);
+        return null;
+    }
+  }
+    
+}
